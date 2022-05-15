a0593518b0bbbdde66a0ad38c115a7bede85739b
ProgrammerJM <johnmarktizado@gmail.com>
2022-05-15 14:02:24 +0800

    first commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,14 +6,14 @@ import { } from "firebase/analytics";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtIvLL37Ux7W7bdawxlr_a0daEGljPypk",
-  authDomain: "medmeet-42bbe.firebaseapp.com",
-  projectId: "medmeet-42bbe",
-  storageBucket: "medmeet-42bbe.appspot.com",
-  messagingSenderId: "302041501078",
-  appId: "1:302041501078:web:ae3449776a04bbdef821b7",
-  measurementId: "G-WHJPS7YCWV",
-
+  apiKey: "AIzaSyC5hJy6e0Dje0BTjOdGPGCe5fKwtj2DSLc",
+  authDomain: "medmeet-f5c53.firebaseapp.com",
+  databaseURL: "https://medmeet-f5c53-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "medmeet-f5c53",
+  storageBucket: "medmeet-f5c53.appspot.com",
+  messagingSenderId: "93507721307",
+  appId: "1:93507721307:web:1420560b61d8677814c77e",
+  measurementId: "G-WBB0MXP4BN"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -21,10 +21,6 @@ export const auth = firebase.auth();
 export const db = firebase.firestore();
 export const storage = firebase.storage();
 export default firebase;
-
-
-
-
 
 
 /* 
